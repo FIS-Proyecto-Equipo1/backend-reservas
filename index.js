1fe5e65d37b0c222c95386ebc63f755df43ba5c3
Roberto <roberto.serrano@babel.es>
2021-01-03 14:00:17 +0100

    Swagger y endpoint de prueba

--- index.js
+++ index.js
@@ -44,4 +44,32 @@ app.get(BASE_API_PATH + "/reservas", (req, res) => {
     res.send(result)
 });
 
+
+
+
+
+//** Dejar para pruebas de autenticación - INICIO */
+var reservations_aux = [
+    {"idCliente": 1, "idVehiculo": 1, "expiracionDatetime": "2020-12-18T13:45:30"}
+]
+
+app.get(BASE_API_PATH + "/pruebas-auth", (req, res) => {
+    console.log(req.headers)
+    idCliente = req.header('x-user')
+    if (idCliente){
+        var result = [];
+        reservations_aux.forEach( _reserva => {
+            if( _reserva.idCliente == idCliente){
+                result.push(_reserva)
+            }
+        });
+        res.send(result)
+    }else{
+        res.status(400).send()
+    }
+});
+//** Dejar para pruebas de autenticación - FIN*/
+
+
+
 app.listen(port);
